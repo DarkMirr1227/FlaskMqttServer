fea53d9f619b8a5d365140865931e8eb2501ae3b
lee sangyeon <mpdark1227@gmail.com>
2022-06-16 14:31:51 +0900

    add DataManagement Function

--- DataManagement.py
+++ DataManagement.py
@@ -1,4 +1,6 @@
 class DataManagement :
+    __groupList__ = list()
+
     def classificationById(self,_list,_id):
         '''리스트에서 해당 id값만 있는 값을 새로운 리스트로 분류
             args:
@@ -12,4 +14,22 @@ class DataManagement :
             if item['id']==_id:
                 new_list.append(item)
         return new_list
-        
+
+    def setGroup(self,_list): 
+        '''요건에 맞게 해당 번호(id)끼리 묶어서 그룹으로 묶어주는 함수 + 동적으로 리스트 생성
+            args:
+                _list =그룹으로 묶일 id(int)를 element로 가진 리스트들의 묶음(리스트) ex) ((1,2,3),(4,5))
+        '''
+        __groupList__ = list() # 전역변수 초기화
+        for group in _list:
+            tempGroup = list()
+            for item in group:
+                tempGroup.append(self.classficationById(item))
+            __groupList__.append(tempGroup)
+                
+
+
+
+
+
+
